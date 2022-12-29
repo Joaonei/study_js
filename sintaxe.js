bfe41d52230698ab39eb5834bf9ba26cb2c504a6
Joao Nei <joaonfagundes26@gmail.com>
2022-12-29 05:49:08 -0300

    Adicionando Sintaxe

---
+++ sintaxe.js
@@ -0,0 +1,24 @@
+// comentários em // ou 
+/* assim
+*/
+    //Declarações
+var
+Declare uma variável, opcionalmente, inicializando-a com um valor.
+
+Experimental let
+Declara uma variável local de escopo do bloco, opcionalmente, inicializando-a com um valor.
+
+Experimental const
+Declara uma constante de escopo de bloco, apenas de leitura.
+
+    //Tipos de dados
+O mais recente padrão ECMAScript define sete tipos de dados:
+
+Seis tipos de dados são os chamados primitivos :
+Boolean . truee false.
+null . Uma palavra-chave que indica valor nulo. Devido ao JavaScript ser case-sensitive, nullnão é o mesmo que Null, NULL, ou ainda outra variação.
+Undefined . Uma propriedade superior cujo valor é indefinido.
+Number . 42ou 3.14159.
+String . "Olá"
+Simbol (novo em ECMAScript 6). Um tipo de dado cujas instâncias são únicas e imutáveis.
+e objeto
