96b8910f10123ff50cca206132c59f6cc223b1d6
Joao Nei <joaonfagundes26@gmail.com>
2023-01-12 16:22:15 -0300

    formatação de string

--- formatacao_de_texto.js
+++ formatacao_de_texto.js
@@ -57,5 +57,65 @@ var splitString = minhaString.split(' ')
 console.log(splitString.slice(2,6))
 
 //substring, substr	
-//Retorna um subconjunto específico de uma string, 
-//definindo os índices inicial e final, ou definindo um índice e um tamanho.
+//Retorna um subconjunto específico de uma string,
+// definindo os índices inicial e final, ou definindo um índice e um tamanho.
+
+
+var minhaString = "The quick brown fox jumps over the lazy dog."
+var subStg = minhaString.substring(0,3)
+console.log(subStg)
+
+var minhaString = "The quick brown fox jumps over the lazy dog."
+var subStg = minhaString.substr(0,3)
+console.log(subStg)
+
+//match, replace, search	
+//Trabalha com expressões regulares.
+
+//procura
+
+var minhaString = "The quick brown fox jumps over the lazy dog."
+var matchStg = minhaString.match("brown")
+console.log(matchStg)
+
+//substitui
+
+var minhaString = "The quick brown fox jumps over the lazy dog."
+var replaceStg = minhaString.replace("brown","João")
+console.log(replaceStg)
+
+//mostar o número da posição
+var minhaString = "The quick brown fox jumps over the lazy dog."
+var searchStg = minhaString.search("the")
+console.log(searchStg)
+
+//procurar a palavra
+let str = "The quick brown fox jumps over the lazy dog.";
+let result = str.match(/[a-z]ox/g);
+console.log(result);
+
+// normalize
+//Retorna a Forma Normalizada Unicode 
+//(Unicode Normalization Form) da string que chama o método.
+
+var minhaString = "Amanhã"
+var normalizeStg = minhaString.normalize("NFD")
+console.log(normalizeStg)
+
+//repeat
+//Retorna uma string contendo os elementos do objeto repetidos pela quantidade de vezes dada.
+
+var minhaString = "The quick brown fox jumps over the lazy dog."
+var repeatStg = minhaString.repeat(3)
+console.log(repeatStg)
+
+//trim
+//Retira espaços em branco no começo e no final da string.
+var minhaString = " The quick brown fox jumps over the lazy dog. "
+var trimStg = minhaString.trim()
+console.log(trimStg)
+
+//  Multi-lines
+var minhaString = " The quick brown fox jumps \n\ over the lazy dog. "
+var trimStg = minhaString.trim()
+console.log(trimStg)
