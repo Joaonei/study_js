869ad7a8b28fea48c4cfa68ccb507973212be688
Joao Nei <joaonfagundes26@gmail.com>
2023-01-13 13:17:57 -0300

    Expressões Regulares

--- regex.js
+++ regex.js
@@ -1,7 +1,5 @@
 const texto = 'Qual a história da música João e Maria? \n\ A história da música João e Maria começa em 1947, quando Chico Buarque tinha apenas três anos. Isso porque a melodia foi composta pelo mestre Sivuca nessa época e, originalmente, era mais pesada, melodiosa e romântica.'
 
-console.log(texto)
 
-module.exports = {
-    texto
-}
+
+module.exports = {texto}
