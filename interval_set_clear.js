46b9d231bdf6ab6da6fe065ef464a02e9aa475d6
Joao Nei <joaonfagundes26@gmail.com>
2023-01-23 18:55:25 -0300

    Intervalos

---
+++ interval_set_clear.js
@@ -0,0 +1,29 @@
+let timer;
+
+function comecar() {
+    timer = setInterval(showTime, 1000);
+
+}
+function terminar() {
+clearInterval(timer)
+
+}
+
+
+
+
+function showTime() {
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let txt = `${h} : ${m} : ${s}`
+
+    document.querySelector('.demo').innerHTML = txt;
+}
+
+
+
+
+document.querySelector('.comecar').addEventListener('click', comecar )
+document.querySelector('.terminar').addEventListener('click',terminar )
