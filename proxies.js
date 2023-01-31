142bacd5625800f4f0f8f08924aa0ea229bda91f
Joao Nei <joaonfagundes26@gmail.com>
2023-01-31 08:49:56 -0300

    Metaprogramação

--- proxies.js
+++ proxies.js
@@ -1,5 +1,25 @@
+//Introduzido em ECMAScript 6, objetos Proxy permitem que você intercepte determinadas operações e implementar comportamentos personalizados.
+// Por exemplo, receber uma propriedade em um objeto:
+
+
 const handler = {
-    get : (target, key) => {
-        console.log(target, key)
+    get: (target, key) => {
+        //        console.log(target, key)
+        if (target[key]) {
+            return target[key]
+        }
+        return "key não enconrada"
+    },
+    set: (target, key, value) => {
+        if (value !== 'João'){
+            target[key] = value
+        }
     }
 }
+const alvo = {};
+const alvoProxy = new Proxy(alvo, handler)
+
+alvoProxy.teste = 'aaa'
+alvoProxy.teste2 = "João"
+
+console.log('log', alvoProxy)
